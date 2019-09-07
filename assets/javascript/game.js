ca5d11dad80742c3b9480b3ef30f3d5de2e4b3ef
Jake Garlick <jacobgarlick@msn.com>
2019-09-07 13:57:30 -0600

    small fixes towards game creation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 // Global Variables
-var gameChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+// var gameChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+var gameChoices = ["a", "p"]
 var wins = 0;
 var losses = 0;
 var guessRemaining = 15;
+var letterGuessCount = [];
 
 // Var for HTML rewrite
 var winsText = document.getElementById("wins");
@@ -11,28 +13,32 @@ var guessNumText = document.getElementById("guess-amount");
 var userChoiceText = document.getElementById("user-choice");
 var letterGuessCountText = document.getElementById("past-choice");
 
-// Event Watcher
-var letterGuessCount = [];
+// Event Watcher & Game Launch 
 
-document.onkeyup = function (event) {
+if (guessRemaining > 0) {
 
-    var userGuess = event.key;
-    var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)]
+    var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+    console.log("comp " + computerGuess);
 
-    letterGuessCount.push(userGuess);
+    document.onkeyup = function (event) {
 
-    if (userGuess === computerGuess) {
-        wins++;
-    } else {
-        guessRemaining--;
+        var userGuess = event.key;
+        console.log("user " + userGuess);
+
+        letterGuessCount.push(userGuess);
+
+        // Write to HTML
+        userChoiceText.textContent = "You chose: " + userGuess;
+        winsText.textContent = "Wins: " + wins;
+        lossesText.textContent = "Losses: " + losses;
+        guessNumText.textContent = "Guesses remaining: " + guessRemaining;
+        letterGuessCountText.textContent = "Your previous choices included: " + letterGuessCount;
+
+        if (userGuess === computerGuess) {
+            wins++;
+        } else {
+            guessRemaining--;
+        }
     }
 
-    // Write to HTML
-    userChoiceText.textContent = "You chose: " + userGuess;
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
-    guessNumText.textContent = "Guesses remaining: " + guessRemaining;
-    letterGuessCountText.textContent = "Your previous choices included: " + letterGuessCount;
-
 }
-
