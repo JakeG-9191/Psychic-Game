60c4fe671f437ddb314c22a14a5d408bf0e0ae6f
Jake Garlick <jacobgarlick@msn.com>
2019-09-09 19:47:35 -0600

    added additional styling for game, updated script files and content for near final product

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var gameChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
 var wins = 0;
 var losses = 0;
-var guessRemaining = 15;
+var guessRemaining = 10;
 var letterGuessCount = [];
 
 // Var for HTML rewrite
@@ -13,41 +13,40 @@ var userChoiceText = document.getElementById("user-choice");
 var otherLetterText = document.getElementById("other-letters");
 var letterGuessCountText = document.getElementById("past-choice");
 
+// Game launches with 1 computer choice for random letter selected 
 var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)];
 console.log("comp " + computerGuess);
 
-// Event Watcher & Game Launch 
+// User presses key to begin playing game
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
     console.log("user " + userGuess);
-
-    // letterGuessCount.push(userGuess);
-
-    if (gameChoices.includes(userGuess) || guessRemaining >= 0) {
-
+    // User must have guesses remaining to play
+    if (gameChoices.includes(userGuess) || guessRemaining > 1) {
+        // User guess is pushed to array to display as previous choice 
         letterGuessCount.push(userGuess);
-
+        // If user guesses correct, move win 1, or if guesses runs out then add 1 to loss, either case engage game reset 
         if (userGuess === computerGuess) {
             wins++;
             gameReset();
-        } else if (guessRemaining === 0) {
+        } else if (guessRemaining === 1) {
             losses++;
             gameReset();
         } else {
             guessRemaining--;
         }
 
-        // Write to HTML
+        // Write to HTML for user information 
         userChoiceText.textContent = "You chose: " + userGuess;
         winsText.textContent = "Wins: " + wins;
         lossesText.textContent = "Losses: " + losses;
         guessNumText.textContent = "Guesses remaining: " + guessRemaining;
         otherLetterText.textContent = "Your Previous Choices Included: "
         letterGuessCountText.textContent = letterGuessCount;
-
+        // Game function reset to continue game after win or loss 
         function gameReset() {
-            (guessRemaining = 15);
+            (guessRemaining = 10);
             (computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)]);
             console.log("comp 2 " + computerGuess);
             (letterGuessCount = []);
